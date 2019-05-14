6de81e5f2f776d57496cc2bb86db7542769e7f30
Logan <vaserk@gmail.com>
2019-05-14 17:14:13 +0800

    lengh between 2-50

--- read.py
+++ read.py
@@ -8,4 +8,11 @@ sum_len = 0
 for d in data:
 	sum_len += len(d)
 print (sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d.strip())>2 and len(d.strip())<50:
+		new.append(d.strip())
+		print (new)
+print ('一共有',len(new),'筆留言長度介於2到50之間')
 		
